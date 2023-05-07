b435df538eba704b676afe747e15a7e9ebcebdfe
nalma <nalma@Nabil>
2023-05-07 12:50:58 +0300

    First Commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int add (int x, int y) {
+       return x + y; 
+    }
+ 
+}
